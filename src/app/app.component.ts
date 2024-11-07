c2a3356fb0dcfea0ac6eb86ee60258c0327b7882
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-07 15:52:31 +0700

    Handle add job

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import type { Job } from './models';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
+  jobs: Job[] = [];
+  jobInput: string = '';
+
+  addJob() {
+    if (!this.jobInput) return;
+    this.jobs.push({
+      title: this.jobInput,
+    });
+  }
 }
