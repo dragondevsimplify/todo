558e3ab925b3ea3f5d9159785018b4acd7079a52
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-07 15:05:05 +0700

    Add todo UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'card': '0 2px 4px #0003, 0 25px 50px #0000001a'
+      }
+    },
   },
   plugins: [],
 }
