4ee1f9ea7c51ad11140d5d28afddb5701c13ebb6
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-07 16:19:43 +0700

    Handle toggle job complete, delete job and get jobs by status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,29 @@ export class AppComponent {
   jobs: Job[] = [];
   jobInput: string = '';
 
+  getJobsByStatus(status: 'all' | 'active' | 'completed') {
+    if (status === 'all') {
+      return this.jobs
+    }
+
+    return this.jobs.filter(i => status === 'active' ? !i.completed : i.completed)
+  }
+
   addJob() {
     if (!this.jobInput) return;
+
     this.jobs.push({
       title: this.jobInput,
     });
+
+    this.jobInput = '';
+  }
+
+  toggleJobComplete(job: Job) {
+    job.completed = !job.completed;
+  }
+
+  deleteJob(idx: number) {
+    this.jobs.splice(idx, 1);
   }
 }
