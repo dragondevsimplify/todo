882a41671722216390ae132cc19a7d524cc1883b
dragondevsimplify <dragondevsimplify@gmail.com>
2024-11-08 09:24:55 +0700

    Handle content editable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import type { Job, JobStatus } from './models';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -13,6 +13,9 @@ export class AppComponent {
   jobs: Job[] = [];
   jobInput = '';
   activeTabName: JobStatus = 'all'
+  allCompleted = false
+
+  @ViewChild('jobsUl', { static: true}) jobsUl!: ElementRef
 
   getJobsByStatus(status: JobStatus) {
     if (status === 'all') {
@@ -36,6 +39,7 @@ export class AppComponent {
 
   toggleJobComplete(job: Job) {
     job.completed = !job.completed;
+    this.allCompleted = this.isAllJobsCompleted()
   }
 
   deleteJob(idx: number) {
@@ -51,17 +55,63 @@ export class AppComponent {
   }
 
   toggleAllJobComplete() {
-    let someCompleted = false, allCompleted = true
+    if (!this.jobs.length) {
+      return
+    }
+
+    let someCompleted = false
+    this.allCompleted = true
     for (const job of this.jobs) {
       if (job.completed) {
         someCompleted = true
       } else {
-        allCompleted = false
+        this.allCompleted = false
       }
     }
 
     this.jobs.forEach(job => {
-      job.completed = (someCompleted && !allCompleted) ? true : !allCompleted
+      job.completed = (someCompleted && !this.allCompleted) ? true : !this.allCompleted
+    })
+
+    this.allCompleted = this.isAllJobsCompleted()
+  }
+
+  isAllJobsCompleted() {
+    return this.jobs.every(i => i.completed)
+  }
+
+  editJob(e: Event) {
+    const target = e.target as HTMLElement
+
+    if (target?.localName !== 'span') {
+      return
+    }
+
+    target.contentEditable = 'true'
+    target.focus()
+  }
+
+  clickOutside(e: Event) {
+    const target = e.target as HTMLElement
+    let targetJobIndex: number
+
+    if (target?.localName === 'span') {
+      targetJobIndex = +target.dataset['jobindex']!
+    }
+
+    const { nativeElement } = this.jobsUl
+    if (!nativeElement) {
+      return
+    }
+
+    [...nativeElement.children].forEach(li => {
+      const span = li.querySelector('span') as HTMLSpanElement
+      if (!span) {
+        return
+      }
+
+      const jobIndex = +span.dataset['jobindex']!
+      span.contentEditable = (jobIndex === targetJobIndex) ? 'true' : 'false'
     })
   }
 }
